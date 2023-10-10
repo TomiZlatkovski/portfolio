2c6cfc2bb05bbd05fdc3a89acd8a5096f0515157
Freeeeeky <tomizlatkovski6911@gmail.com>
2023-10-10 02:46:40 +0200

    GH-PAGES Deploy

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -3,8 +3,12 @@ import '../styles/LandingPage.css'
 export default function LandingPage() {
     return (
         <div className="page">
-            <h1 className="titleText">🛠️ Website under construction! 🛠️</h1>
-            <img className="image" src="/images/ManPushingRock.jpg" />
+            <span className='titleText'>
+                <i className="fa-solid fa-hammer fa-2xl" style={{ color: "#c9701d;" }}></i>
+                Website Under Construction
+                <i className="fa-solid fa-hammer fa-flip-horizontal fa-2xl" style={{color: "#c9701d;"}}></i>
+            </span>
+            <img className="image" src="images/ManPushingRock.jpg" />
         </div>
     )
 }
