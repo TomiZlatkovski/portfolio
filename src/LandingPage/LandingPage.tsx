62f5b39c577b24cee7187c614a5b89dc43d4ff2a
Freeeeeky <tomizlatkovski6911@gmail.com>
2023-10-14 02:49:20 +0200

    Fixing branch

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -2,13 +2,14 @@ import '../styles/LandingPage.css'
 
 export default function LandingPage() {
     return (
-        <div className="page">
-            <span className='titleText'>
-                <i className="fa-solid fa-hammer fa-2xl" style={{ color: "#c9701d;" }}></i>
-                Website Under Construction
-                <i className="fa-solid fa-hammer fa-flip-horizontal fa-2xl" style={{color: "#c9701d;"}}></i>
-            </span>
-            <img className="image" src="images/ManPushingRock.jpg" />
-        </div>
+        <>
+            <section className='page' id='first-page'>
+                <span className='titleText'>Welcome To My Portfolio Webiste!</span>
+                <a href='#second-page'><button className='smooth-scroll-button'>View my Work <i className="fa-solid fa-arrow-right"></i></button></a>
+            </section>
+            <section className='page' id="second-page">
+                <h1>Second Section of Page</h1>
+            </section>
+        </>
     )
 }
